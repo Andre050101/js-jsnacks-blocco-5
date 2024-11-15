68a604ea1c2a4ab9be9cb6825f1fec9aab0daef7
Andre050101 <agiancristiano90@gmail.com>
2024-11-15 16:02:19 +0100

    snack 8 done

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const classeMarcoLanci = students.find((student) => {
+  return student.name === "Marco Lanci";
+});
+
+console.log(classeMarcoLanci.class);
